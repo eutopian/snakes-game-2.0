9eb06ca007f792d39e29ac339e9581e925b44461
Margaret Ma <eutopia@gmail.com>
2018-03-24 12:24:20 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+// the function to move will be on the head. The app contains the state of where the head/ body / apple is at all times
+// the apple will ahve a function to gennerate a new apple when it is eatten
+// thee body will have a function to gneeratee a neew body wheenn it eats an apple
+
+
+export default App;
